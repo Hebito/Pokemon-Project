1214ec01a7af938102383081a09d4857bb1cb364
Ricky Lazo <arcticrupture@gmail.com>
2021-04-28 17:05:04 -0400

    importing Pokemon Project

---
+++ PokemonAPI - Lazo/PokemonAPI - Lazo/PokeDexForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace PokemonAPI___Lazo
+{
+    partial class PokeDexForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Species = new System.Windows.Forms.Button();
+            this.txt_Species = new System.Windows.Forms.TextBox();
+            this.label_PokemonName = new System.Windows.Forms.Label();
+            this.label_CaptureRate = new System.Windows.Forms.Label();
+            this.txt_CaptureRate = new System.Windows.Forms.TextBox();
+            this.label_BaseHappiness = new System.Windows.Forms.Label();
+            this.txt_BaseHappiness = new System.Windows.Forms.TextBox();
+            this.label_GrowthRate = new System.Windows.Forms.Label();
+            this.txt_GrowthRate = new System.Windows.Forms.TextBox();
+            this.label_EggGroup = new System.Windows.Forms.Label();
+            this.txt_EggGroup = new System.Windows.Forms.TextBox();
+            this.label_Habitat = new System.Windows.Forms.Label();
+            this.txt_Habitat = new System.Windows.Forms.TextBox();
+            this.label_FlavorText1 = new System.Windows.Forms.Label();
+            this.label_FlavorText2 = new System.Windows.Forms.Label();
+            this.txt_IdSubmitter = new System.Windows.Forms.TextBox();
+            this.label_Instruction = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Species
+            // 
+            this.btn_Species.ForeColor = System.Drawing.Color.Green;
+            this.btn_Species.Location = new System.Drawing.Point(372, 76);
+            this.btn_Species.Name = "btn_Species";
+            this.btn_Species.Size = new System.Drawing.Size(75, 23);
+            this.btn_Species.TabIndex = 0;
+            this.btn_Species.Text = "Submit";
+            this.btn_Species.UseVisualStyleBackColor = true;
+            this.btn_Species.Click += new System.EventHandler(this.btn_Species_Click);
+            // 
+            // txt_Species
+            // 
+            this.txt_Species.Location = new System.Drawing.Point(151, 139);
+            this.txt_Species.Name = "txt_Species";
+            this.txt_Species.ReadOnly = true;
+            this.txt_Species.Size = new System.Drawing.Size(100, 20);
+            this.txt_Species.TabIndex = 1;
+            // 
+            // label_PokemonName
+            // 
+            this.label_PokemonName.AutoSize = true;
+            this.label_PokemonName.Location = new System.Drawing.Point(52, 142);
+            this.label_PokemonName.Name = "label_PokemonName";
+            this.label_PokemonName.Size = new System.Drawing.Size(93, 13);
+            this.label_PokemonName.TabIndex = 2;
+            this.label_PokemonName.Text = "Pokemon\'s Name:";
+            // 
+            // label_CaptureRate
+            // 
+            this.label_CaptureRate.AutoSize = true;
+            this.label_CaptureRate.Location = new System.Drawing.Point(66, 259);
+            this.label_CaptureRate.Name = "label_CaptureRate";
+            this.label_CaptureRate.Size = new System.Drawing.Size(73, 13);
+            this.label_CaptureRate.TabIndex = 4;
+            this.label_CaptureRate.Text = "Capture Rate:";
+            // 
+            // txt_CaptureRate
+            // 
+            this.txt_CaptureRate.Location = new System.Drawing.Point(151, 256);
+            this.txt_CaptureRate.Name = "txt_CaptureRate";
+            this.txt_CaptureRate.ReadOnly = true;
+            this.txt_CaptureRate.Size = new System.Drawing.Size(100, 20);
+            this.txt_CaptureRate.TabIndex = 3;
+            // 
+            // label_BaseHappiness
+            // 
+            this.label_BaseHappiness.AutoSize = true;
+            this.label_BaseHappiness.Location = new System.Drawing.Point(52, 201);
+            this.label_BaseHappiness.Name = "label_BaseHappiness";
+            this.label_BaseHappiness.Size = new System.Drawing.Size(87, 13);
+            this.label_BaseHappiness.TabIndex = 6;
+            this.label_BaseHappiness.Text = "Base Happiness:";
+            // 
+            // txt_BaseHappiness
+            // 
+            this.txt_BaseHappiness.Location = new System.Drawing.Point(151, 198);
+            this.txt_BaseHappiness.Name = "txt_BaseHappiness";
+            this.txt_BaseHappiness.ReadOnly = true;
+            this.txt_BaseHappiness.Size = new System.Drawing.Size(100, 20);
+            this.txt_BaseHappiness.TabIndex = 5;
+            // 
+            // label_GrowthRate
+            // 
+            this.label_GrowthRate.AutoSize = true;
+            this.label_GrowthRate.Location = new System.Drawing.Point(527, 201);
+            this.label_GrowthRate.Name = "label_GrowthRate";
+            this.label_GrowthRate.Size = new System.Drawing.Size(70, 13);
+            this.label_GrowthRate.TabIndex = 12;
+            this.label_GrowthRate.Text = "Growth Rate:";
+            // 
+            // txt_GrowthRate
+            // 
+            this.txt_GrowthRate.Location = new System.Drawing.Point(612, 198);
+            this.txt_GrowthRate.Name = "txt_GrowthRate";
+            this.txt_GrowthRate.ReadOnly = true;
+            this.txt_GrowthRate.Size = new System.Drawing.Size(100, 20);
+            this.txt_GrowthRate.TabIndex = 11;
+            // 
+            // label_EggGroup
+            // 
+            this.label_EggGroup.AutoSize = true;
+            this.label_EggGroup.Location = new System.Drawing.Point(536, 259);
+            this.label_EggGroup.Name = "label_EggGroup";
+            this.label_EggGroup.Size = new System.Drawing.Size(61, 13);
+            this.label_EggGroup.TabIndex = 10;
+            this.label_EggGroup.Text = "Egg Group:";
+            // 
+            // txt_EggGroup
+            // 
+            this.txt_EggGroup.Location = new System.Drawing.Point(612, 256);
+            this.txt_EggGroup.Name = "txt_EggGroup";
+            this.txt_EggGroup.ReadOnly = true;
+            this.txt_EggGroup.Size = new System.Drawing.Size(100, 20);
+            this.txt_EggGroup.TabIndex = 9;
+            // 
+            // label_Habitat
+            // 
+            this.label_Habitat.AutoSize = true;
+            this.label_Habitat.Location = new System.Drawing.Point(556, 142);
+            this.label_Habitat.Name = "label_Habitat";
+            this.label_Habitat.Size = new System.Drawing.Size(44, 13);
+            this.label_Habitat.TabIndex = 8;
+            this.label_Habitat.Text = "Habitat:";
+            // 
+            // txt_Habitat
+            // 
+            this.txt_Habitat.Location = new System.Drawing.Point(612, 139);
+            this.txt_Habitat.Name = "txt_Habitat";
+            this.txt_Habitat.ReadOnly = true;
+            this.txt_Habitat.Size = new System.Drawing.Size(100, 20);
+            this.txt_Habitat.TabIndex = 7;
+            // 
+            // label_FlavorText1
+            // 
+            this.label_FlavorText1.AutoSize = true;
+            this.label_FlavorText1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_FlavorText1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label_FlavorText1.Location = new System.Drawing.Point(127, 318);
+            this.label_FlavorText1.Name = "label_FlavorText1";
+            this.label_FlavorText1.Size = new System.Drawing.Size(100, 18);
+            this.label_FlavorText1.TabIndex = 13;
+            this.label_FlavorText1.Text = "Description";
+            // 
+            // label_FlavorText2
+            // 
+            this.label_FlavorText2.AutoSize = true;
+            this.label_FlavorText2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label_FlavorText2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label_FlavorText2.Location = new System.Drawing.Point(500, 318);
+            this.label_FlavorText2.Name = "label_FlavorText2";
+            this.label_FlavorText2.Size = new System.Drawing.Size(100, 18);
+            this.label_FlavorText2.TabIndex = 14;
+            this.label_FlavorText2.Text = "Description";
+            // 
+            // txt_IdSubmitter
+            // 
+            this.txt_IdSubmitter.Location = new System.Drawing.Point(360, 39);
+            this.txt_IdSubmitter.Name = "txt_IdSubmitter";
+            this.txt_IdSubmitter.Size = new System.Drawing.Size(100, 20);
+            this.txt_IdSubmitter.TabIndex = 15;
+            // 
+            // label_Instruction
+            // 
+            this.label_Instruction.AutoSize = true;
+            this.label_Instruction.ForeColor = System.Drawing.Color.Black;
+            this.label_Instruction.Location = new System.Drawing.Point(231, 42);
+            this.label_Instruction.Name = "label_Instruction";
+            this.label_Instruction.Size = new System.Drawing.Size(123, 13);
+            this.label_Instruction.TabIndex = 16;
+            this.label_Instruction.Text = "Type in a Pokemon\'s ID:";
+            // 
+            // PokeDexForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_Instruction);
+            this.Controls.Add(this.txt_IdSubmitter);
+            this.Controls.Add(this.label_FlavorText2);
+            this.Controls.Add(this.label_FlavorText1);
+            this.Controls.Add(this.label_GrowthRate);
+            this.Controls.Add(this.txt_GrowthRate);
+            this.Controls.Add(this.label_EggGroup);
+            this.Controls.Add(this.txt_EggGroup);
+            this.Controls.Add(this.label_Habitat);
+            this.Controls.Add(this.txt_Habitat);
+            this.Controls.Add(this.label_BaseHappiness);
+            this.Controls.Add(this.txt_BaseHappiness);
+            this.Controls.Add(this.label_CaptureRate);
+            this.Controls.Add(this.txt_CaptureRate);
+            this.Controls.Add(this.label_PokemonName);
+            this.Controls.Add(this.txt_Species);
+            this.Controls.Add(this.btn_Species);
+            this.ForeColor = System.Drawing.Color.DarkOrange;
+            this.Name = "PokeDexForm";
+            this.Text = "Pokemon Species";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Species;
+        private System.Windows.Forms.TextBox txt_Species;
+        private System.Windows.Forms.Label label_PokemonName;
+        private System.Windows.Forms.Label label_CaptureRate;
+        private System.Windows.Forms.TextBox txt_CaptureRate;
+        private System.Windows.Forms.Label label_BaseHappiness;
+        private System.Windows.Forms.TextBox txt_BaseHappiness;
+        private System.Windows.Forms.Label label_GrowthRate;
+        private System.Windows.Forms.TextBox txt_GrowthRate;
+        private System.Windows.Forms.Label label_EggGroup;
+        private System.Windows.Forms.TextBox txt_EggGroup;
+        private System.Windows.Forms.Label label_Habitat;
+        private System.Windows.Forms.TextBox txt_Habitat;
+        private System.Windows.Forms.Label label_FlavorText1;
+        private System.Windows.Forms.Label label_FlavorText2;
+        private System.Windows.Forms.TextBox txt_IdSubmitter;
+        private System.Windows.Forms.Label label_Instruction;
+    }
+}
+
---
+++ PokemonAPI - Lazo/PokemonAPI - Lazo/PokeDexForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PokeAPI;
+using Newtonsoft.Json;
+
+
+
+namespace PokemonAPI___Lazo
+{
+    public partial class PokeDexForm : Form
+    {
+        public PokeDexForm()
+        {
+            InitializeComponent();
+        }
+
+        public async void GetSpecies(int PokemonFindID)
+        {
+            PokemonSpecies p = await DataFetcher.GetApiObject<PokemonSpecies>(PokemonFindID);
+            txt_Species.Text = p.Name;
+            txt_BaseHappiness.Text = p.BaseHappiness.ToString();
+            txt_CaptureRate.Text = p.CaptureRate.ToString();
+            label_FlavorText1.Text = p.FlavorTexts[0].FlavorText;
+            label_FlavorText2.Text = p.FlavorTexts[2].FlavorText;
+            txt_GrowthRate.Text = p.GrowthRate.Name;
+            txt_Habitat.Text = p.Habitat.Name;
+            txt_EggGroup.Text = p.EggGroups[0].Name;
+
+        }
+
+        private void btn_Species_Click(object sender, EventArgs e)
+        {
+            int PokemonID;
+            try
+            {
+                PokemonID = int.Parse(txt_IdSubmitter.Text);
+
+                if (PokemonID > 0 && PokemonID < 387)
+                {
+                    GetSpecies(PokemonID);
+                }
+                else
+                {
+                     MessageBox.Show("The ID entered is not valid, please try again!");
+                }
+            }
+
+            catch (FormatException)
+            {
+                MessageBox.Show("The ID entered is not valid, please try again!");
+            }  
+        }
+    }
+}
